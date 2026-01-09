02f7e1029230495264cfb64e5bfab4a2258271c5
Deepti Karambelkar <deepti.karambelkar2016@gmail.com>
2026-01-09 14:04:48 -0800

    Add project files.

---
+++ EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeController() { }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            List<Employee> employees = new List<Employee>(){
+            new Employee { Id = 1, Name = "Deepti Karambelkar", Address = "test address", Email = "test@gmail.com", Phone = "1234567890" },
+            new Employee { Id = 2, Name = "Aarya Karambelkar", Address = "test address", Email = "test@gmail.com", Phone = "1234567890" },
+            new Employee { Id = 3, Name = "Shreya Karambelkar", Address = "test address", Email = "test@gmail.com", Phone = "1234567890" }
+            };
+            return (IEnumerable<Employee>)employees;
+        }
+
+    }
+}
